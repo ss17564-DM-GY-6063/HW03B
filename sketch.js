063e11fd853efc3a9ff1c81d8ffc7ba78d84aadd
shuoshan <ss17564@nyu.edu>
2023-09-24 22:43:31 -0400

    update (three sizes of ellipses)

--- sketch.js
+++ sketch.js
@@ -8,11 +8,19 @@ function setup() {
 function draw() {
   background(240,215,0);
   fill("black");
-  randomSeed(55);
+  randomSeed(22);
 
   for (let xpos = 0; xpos <= width; xpos += eDiam) {
     for (let ypos = 0; ypos <= height; ypos += eDiam) {
-      let tDiam = random(3,50);
+      let x = random(0, 1);
+      if(x < 0.5) {
+        tDiam = 10;
+      } else if (x < 0.9){
+        tDiam = 30;
+      }
+      else if (x < 1){
+        tDiam = 50;
+      }
       let trans = random(10,80);
       ellipse(xpos + trans, ypos + trans, tDiam, tDiam);
     }
